0f72d4b5376200fd5c9594b1048b556d000622d0
Eoin Molloy <eoinmolloy@Eoins-MacBook-Air.local>
2024-11-27 21:24:41 +0000

    Q1 (b)

--- problem-sets/PS1/src/p01b_logreg.py
+++ problem-sets/PS1/src/p01b_logreg.py
@@ -34,9 +34,61 @@ class LogisticRegression(LinearModel):
             x: Training example inputs. Shape (m, n).
             y: Training example labels. Shape (m,).
         """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
+        # Newtons Method
+        # theta_(t+1) = theta_t + H^(-1)*grad_theta(l)
+        def h(theta, x):
+            """Vectorized implementation of h_theta(x) = 1 / (1 + exp(-theta^T x)).
 
+            :param theta: Shape (n,).
+            :param x:     All training examples of shape (m, n).
+            :return:      The hypothesis for all training examples. Shape (m,).
+            """
+            return 1 / (1 + np.exp(-np.dot(x, theta)))
+
+        def gradient(theta, x, y):
+            """Vectorized implementation of the gradient of J(theta).
+
+            :param theta: Shape (n,).
+            :param x:     All training examples of shape (m, n).
+            :param y:     All labels of shape (m,).
+            :return:      The gradient of shape (n,).
+            """
+            m, _ = x.shape
+            return -1 / m * np.dot(x.T, (y - h(theta, x)))
+
+        def hessian(theta, x):
+            """Vectorized implementation of the Hessian of J(theta).
+
+            :param theta: Shape (n,).
+            :param x:     All training examples of shape (m, n).
+            :return:      The Hessian of shape (n, n).
+            """
+            m, _ = x.shape
+            h_theta_x = np.reshape(h(theta, x), (-1, 1))
+            return 1 / m * np.dot(x.T, h_theta_x * (1 - h_theta_x) * x)
+
+        def next_theta(theta, x, y):
+            """The next theta updated by Newton's Method.
+
+            :param theta: Shape (n,).
+            :return:      The updated theta of shape (n,).
+            """
+            return theta - np.dot(np.linalg.inv(hessian(theta, x)), gradient(theta, x, y))
+
+        
+        # Initialize theta
+        if self.theta is None:
+            self.theta = np.zeros(n)
+
+        # Update theta using Newton's Method
+        old_theta = self.theta
+        new_theta = next_theta(self.theta, x, y)
+        while np.linalg.norm(new_theta - old_theta, 1) >= self.eps:
+            old_theta = new_theta
+            new_theta = next_theta(old_theta, x, y)
+
+        self.theta = new_theta
+        
     def predict(self, x):
         """Make a prediction given new inputs x.
 
